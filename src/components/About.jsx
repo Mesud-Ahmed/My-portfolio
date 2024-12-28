62df1e1c968eee03f987080a74593494dae2b638
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-12-28 08:23:21 +0300

    refactor: use global css for the background and keep the app component simpl

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+export default function About(){
+    return(
+        <div className="border-b border-neutral-900 pb-4">
+            <h2 className="my-20 text-center text-4xl">About me</h2>
+        </div>
+    )
+}
