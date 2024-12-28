bde5fcd8f972e5057e01b45fc16461c83a391884
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-12-28 07:49:06 +0300

    feat: add about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { HERO_CONTENT } from "../constants"
 export default function Hero() {
     return (
         <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -5,6 +6,8 @@ export default function Hero() {
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
                         <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Mesud Ahmed</h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to bg-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Junior React Developer</span>
+                        <p className="my-2 max-w-xl py-6 font-light ">{HERO_CONTENT}</p>
                     </div>
                 </div>
             </div>
